5283728d329844912ab001ddab2fbcf6130dd1cc
Sublime <other@Alberts-MBP.fios-router.home>
2018-09-19 12:55:01 -0400

    made code in java

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,12 @@
-new file added
+public class CtoFTester{
+  public static double celsiusToFahrenheit(double x){
+    return (x * 9.0/5) + 32;
+}
+  public static double fahrenheitToCelsius(double x){
+    return (x - 32.0) * 5.0/9;
+}
+  public static void main(String[] args){
+    System.out.println(celsiusToFahrenheit(3.0));
+    System.out.println(fahrenheitToCelsius(0.0));
+}
+}
